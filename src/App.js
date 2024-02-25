4809fd579719ce0877426166f6ff0b8202fb3278
Bidhun V K <bidhunvaikkath@gmail.com>
2024-02-25 10:12:25 +0530

    fully functional todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState('');
+
+  const removeTodo = (id) => {
+    const index = toDos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      const newTodos = [...toDos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    }
+  }
+
   return (
     <div className="app">
     <div className="mainHeading">
@@ -11,19 +23,31 @@ function App() {
       <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
     </div>
     <div className="input">
-      <input type="text" placeholder="🖊️ Add item..." />
-      <i className="fas fa-plus"></i>
+      <input value={toDo} onChange={ (e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i className="fas fa-plus" onClick={()=>setTodos([...toDos, {id:Date.now(), text:toDo , status:false}])}></i>
     </div>
     <div className="todos">
-      <div className="todo">
-        <div className="left">
-          <input type="checkbox" name="" id="" />
-          <p>Rect tutorial</p>
-        </div>
-        <div className="right">
-          <i className="fas fa-times"></i>
-        </div>
-      </div>
+        {toDos.map((value) => {
+          return (
+            <div  className={`todo ${value.status ? 'completed' : ''}`}>
+          <div className="left">
+                <input value={value.status} type="checkbox" name="" id="" onChange={(e) => {
+                  setTodos(toDos.filter(obj2 => {
+                    if (obj2.id === value.id) {
+                      obj2.status = e.target.checked
+                    }
+                    return obj2
+                  }))
+            }}/>
+                <p>{ value.text }</p>
+          </div>  
+          <div className="right">
+                <i className="fas fa-times" onClick={() => removeTodo(value.id)}></i>
+          </div>
+            </div>
+          )
+        })
+          }
     </div>
   </div>
 );
